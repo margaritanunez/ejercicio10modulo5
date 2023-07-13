0f6c357fb08bc2c4af0800cd708ba783ac148be2
Margaret <maenunezt@gmail.com>
2023-07-12 21:51:37 -0400

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/Phone.kt
@@ -0,0 +1,14 @@
+package com.example.myapplication
+
+open class Phone(var isScreenLightOn: Boolean = false){
+    open fun switchOn() {
+        isScreenLightOn = true
+    }
+    fun switchOff() {
+        isScreenLightOn = false
+    }
+    fun checkPhoneScreenLight() {
+        val phoneScreenLight = if (isScreenLightOn) "on" else "off"
+        println("The phone screen's light is $phoneScreenLight.")
+    }
+}
